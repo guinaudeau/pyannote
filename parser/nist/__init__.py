fb8bead486e0f67275633943ea33ca807a4980d3
Hervé BREDIN <bredin@limsi.fr>
2012-02-06 08:55:18 +0100

    Parser documentation

--- parser/nist/__init__.py
+++ parser/nist/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""
+This module contains parser for common file formats used in NIST Rich Transcription campaigns.
+See http://www.itl.nist.gov/iad/mig/tests/rt/
+"""
+
 __all__ = ['MDTMParser', 'UEMParser', 'ETFParser', 'ETF0Parser']
 
 from mdtm import MDTMParser
--- parser/repere/__init__.py
+++ parser/repere/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""
+This module contains parser for common file formats used in the REPERE challenge (2011-2013).
+See http://www.defi-repere.fr/
+"""
+
 __all__ = ['REPEREParser', 'TRSParser', 'XGTFParser']
 
 from repere import REPEREParser
