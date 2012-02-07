53feb78ea306878882b24e9718415ffbd7e85bda
Hervé BREDIN <bredin@limsi.fr>
2012-02-07 17:41:17 +0100

    Bug fix in pyannote.metric module imports

--- metric/__init__.py
+++ metric/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-__all__ = ['detection_error_rate', 'der', \
+__all__ = ['diarization_error_rate', 'der', \
            'identification_error_rate', 'ier', \
            'detection_error_rate']
 
-from diarization import detection_error_rate, der
+from diarization import diarization_error_rate, der
 from identification import identification_error_rate, ier
 from detection import detection_error_rate
