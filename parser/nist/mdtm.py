512cb95ae0244a8c4fe78897091871bf16c931db
Hervé BREDIN <bredin@limsi.fr>
2012-02-10 18:17:54 +0100

    New pyannote.parser.nist.mdtm.toMDTM() conversion function

--- parser/nist/mdtm.py
+++ parser/nist/mdtm.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from ..generic import GenericParser
+from pyannote import TrackIDAnnotation
 
 class MDTMParser(GenericParser):
     """
@@ -21,18 +22,41 @@ class MDTMSample(MDTMParser):
         sample_mdtm = '%s/../data/sample.mdtm' % (os.path.dirname(__file__))
         super(MDTMSample, self).__init__(sample_mdtm)
 
-# def toMDTM(annotation):
-#     """Meta Data Time-Mark"""
-#     modality = annotation.modality
-#     video    = annotation.video
-#     text = ''
-#     for s, segment in enumerate(annotation):
-#         start = segment.start
-#         duration = segment.duration
-#         for i, identifier in enumerate(annotation.identifiers(segment=segment)):
-#             confidence = annotation.confidence(segment, identifier)
-#             # source 1 start duration modality confidence subtype identifier
-#             text += '%s 1 %g %g %s %g %s %s\n' % (video, start, duration, modality, confidence, 'unknown', identifier)
-#     return text
-# 
-# 
+
+def toMDTM(annotation, confidence=None):
+    """
+    Meta Data Time-Mark
+    .. currentmodule:: pyannote
+    
+    :param annotation: annotation to be converted to MDTM format 
+    :type annotation: :class:`TrackIDAnnotation` or :class:`IDAnnotation`
+    :param confidence: function that takes annotation[s, t, i] as input and return confidence value
+    :type confidence: function
+
+    :returns: string containing annotation in MDTM format
+    """
+
+    if not isinstance(annotation, TrackIDAnnotation):
+        raise TypeError('')
+    
+    annotation = annotation.toTrackIDAnnotation()
+    
+    if confidence is None:
+        confidence = (lambda x: 1)
+    
+    modality = annotation.modality
+    video    = annotation.video
+    text = ''
+        
+    for segment in annotation:
+        start = segment.start
+        duration = segment.duration    
+        for track in annotation[segment]:
+            for identifier in annotation[segment, track]:
+                score = confidence(annotation[segment, track, identifier])
+                # source 1 start duration modality confidence subtype identifier
+                text += '%s 1 %g %g %s %g %s %s\n' % (video, start, duration, modality, score, track, identifier)
+                    
+    return text
+
+
