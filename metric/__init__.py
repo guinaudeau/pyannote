5998a757d6277ffe2f7e53b52f1efe946cef4b1a
Hervé BREDIN <bredin@limsi.fr>
2012-02-14 17:25:56 +0100

    Added several new metric to pyannote.metric.association
    - homogeneity
    - completeness
    - V-measure
    - adjusted Rand index
    - adjusted mutual information

--- metric/__init__.py
+++ metric/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-__all__ = ['diarization_error_rate', 'der', \
-           'identification_error_rate', 'ier', \
-           'detection_error_rate', \
-           'association_error_rate']
+# __all__ = ['diarization_error_rate', 'der', \
+#            'identification_error_rate', 'ier', \
+#            'detection_error_rate', ]
 
-from diarization import diarization_error_rate, der
-from identification import identification_error_rate, ier
-from detection import detection_error_rate
-from association import association_error_rate
+# from diarization import diarization_error_rate, der
+# from identification import identification_error_rate, ier
+# from detection import detection_error_rate
+# from association import association_error_rate
