576dc4fabae0f620a1c14e1b38f8fdd4533694f2
Hervé BREDIN <bredin@limsi.fr>
2012-02-16 11:10:16 +0100

    Usual copy/paste bug

--- metric/association.py
+++ metric/association.py
@@ -12,7 +12,7 @@ def __get_labels_true_pred(hypothesis, reference=None):
         raise TypeError('Hypothesis must be a Mapping, not %s.' % type(hypothesis).__name__)
     
     if reference and not isinstance(reference, Mapping):
-        raise TypeError('Reference must be either None or a Mapping, not %s.' % type(hypothesis).__name__)
+        raise TypeError('Reference must be either None or a Mapping, not %s.' % type(reference).__name__)
     
     partition = hypothesis.to_partition()
     
@@ -52,55 +52,4 @@ def adjusted_mutual_info(hypothesis, reference=None):
     labels_true, labels_pred = __get_labels_true_pred(hypothesis, reference=reference)        
     return sklearn.metrics.adjusted_mutual_info_score(labels_true, labels_pred)
 
-    
-    
-    
-# def association_error_rate(source, target, mapping, weights=None):
-#     
-#     """
-#     :param source: 
-#     :type source: list or set
-#     :param target:
-#     :type target:
-#     :param mapping: {source --> target} dictionary. 
-#     :type mapping: dictionary
-#     
-#     Typically,
-#         >>> source = modality1.IDs
-#         >>> target = modality2.IDs
-#         >>> mapping = hungarian(source, target)
-#         >>> weights = {s: modality1(s).timeline.duration() for s in source}
-#         >>> association_error_rate(source, target, mapping, weights)
-#     
-#     """
-#     
-#     # list of source IDs that actually have a match in target
-#     should_be_mapped = set(source) & set(target)
-#     
-#     if not weights:
-#         weights = {}
-#         for s in source:
-#             weights[s] = 1
-#     
-#     for s in source:
-#         if s not in mapping:
-#             mapping[s] = NoMatch()
-#     
-#     miss = 0.
-#     fa = 0.
-#     error = 0.
-#     total = 0.
-#     
-#     for s in source:
-#         total += weights[s]
-#         if s in should_be_mapped:
-#             if isinstance(mapping[s], NoMatch):
-#                 miss += weights[s]
-#             else:
-#                 if mapping[s] != s:
-#                     error += weights[s]
-#         elif not isinstance(mapping[s], NoMatch):
-#             fa += weights[s]
-#                 
-#     return 1. * (miss + fa + error) / total
     
