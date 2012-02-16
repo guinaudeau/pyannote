860df2c3e3b9deba5437fe9c3d853df638338b24
Hervé BREDIN <bredin@limsi.fr>
2012-02-16 12:10:58 +0100

    Added new pyannote.metric.association.accuracy metric

--- metric/association.py
+++ metric/association.py
@@ -4,7 +4,7 @@
 """
 """
 import sklearn.metrics
-from pyannote.base.association import Mapping, OneToOneMapping
+from pyannote.base.association import Mapping, OneToOneMapping, NoMatch
 
 def __get_labels_true_pred(hypothesis, reference=None):
     
@@ -52,4 +52,57 @@ def adjusted_mutual_info(hypothesis, reference=None):
     labels_true, labels_pred = __get_labels_true_pred(hypothesis, reference=reference)        
     return sklearn.metrics.adjusted_mutual_info_score(labels_true, labels_pred)
 
+# ------------------------------------ #
+# One-to-one mapping evaluation metric #
+# ------------------------------------ #
+
+def __one_way_accuracy(proposed, expected):
     
+    elements =  [element for element in expected if not isinstance(element, NoMatch)]
+    
+    correct = 0
+    false_alarm = 0
+    error = 0
+    total = len(elements)
+    
+    for element in elements:
+        if isinstance(proposed[element], NoMatch):
+            if isinstance(expected[element], NoMatch):
+                correct += 1
+            else:
+                false_alarm += 1
+        else:
+            if proposed[element] == expected[element]:
+                correct += 1
+            else:
+                error += 1
+    
+    return {'total': total, 'correct': correct, 'false_alarm': false_alarm, 'error': error}
+    
+def __get_dict_proposed_expected(hypothesis, reference=None, reverse=False):
+    
+    if not isinstance(hypothesis, OneToOneMapping):
+        raise TypeError('Hypothesis must be a OneToOneMapping, not %s.' % type(hypothesis).__name__)
+    
+    if reference and not isinstance(reference, OneToOneMapping):
+        raise TypeError('Reference must be either None or a OneToOneMapping, not %s.' % type(reference).__name__)
+        
+    proposed = hypothesis.to_dict(reverse=reverse)
+    if reference:
+        expected = reference.to_dict(reverse=reverse)
+    else:
+        expected = hypothesis.to_expected_dict(reverse=reverse)
+
+    return proposed, expected
+
+
+def accuracy(hypothesis, reference=None):
+    
+    proposed, expected = __get_dict_proposed_expected(hypothesis, reference=reference, reverse=False)
+    l2r = __one_way_accuracy(proposed, expected)
+
+    proposed, expected = __get_dict_proposed_expected(hypothesis, reference=reference, reverse=True)
+    r2l = __one_way_accuracy(proposed, expected)
+    
+    return 1. * (l2r['correct'] + r2l['correct']) / (l2r['total'] + r2l['total'])
+        
