cd966292ed2c7e417cc101aab33fb5f4efb39592
Hervé BREDIN <bredin@limsi.fr>
2012-02-27 21:03:23 +0100

    SEGParser class now inherits from GenericParser

--- parser/other/seg.py
+++ parser/other/seg.py
@@ -18,80 +18,27 @@
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyannote import IDAnnotation, Segment, SlidingWindow
+from ..generic import GenericParser
+from pyannote.base.feature import SlidingWindow
+import warnings
 
-class SEGParser(object):
+class SEGParser(GenericParser):
     """
     .seg file parser
     """
     def __init__(self, path2seg, \
-                       video=None, \
                        modality=None, \
-                       sliding_window=SlidingWindow(), \
-                       ):
+                       multitrack=False, \
+                       sliding_window=SlidingWindow()):
         
-        super(SEGParser, self).__init__()
-        self.path2seg = path2seg
+        warnings.warn('SEGParser never was tested...')
         
-        # read video name from .seg file if needed
-        if video is None:
-            # find video in .seg file
-            # (first field of first uncommented line)
-            f = open(self.path2seg, 'r')
-            for line in f:
-                # skip commented line
-                if line[0] == '#':
-                    continue
-                self.video = line.split()[0]
-                break
-        else:
-            self.video = video
-        
-        # store modality
-        self.modality = modality
-        
-        # if sliding_window is provided, store it for later use
-        # otherwise, assumes .seg files contains timestamps and duration in seconds
-        # (rather than frame id and number)
-        self.sliding_window = sliding_window
-        
-        # empty annotation
-        self.seg = IDAnnotation(modality=self.modality, video=self.video)
-        
-        # parse file
-        f = open(self.path2seg, 'r')
-        for line in f:
-            # skip comments
-            if line[0] == '#':
-                continue
-            # split line into fields
-            fields = line.split()
-            # _video = fields[0]
-            identifier = fields[1]
-            # _sentence = fields[2]
-            raw_i = fields[3]
-            raw_n = fields[4]
-            
-            # compute Segment
-            if self.sliding_window is not None:
-                segment = self.sliding_window.toSegment(int(raw_i) - 1, int(raw_n))
-            else:
-                segment = Segment(float(raw_i), float(raw_i) + float(raw_n))
-            
-            self.seg[segment, identifier] = {'confidence': 1.}
-            #self.seg.add(segment, identifier, attributes={}, confidence=1.)
-    
-    def annotation(self):
-        return self.seg
-        
-    def timeline(self):
-        return self.seg.timeline
-
-class SEGSample(SEGParser):
-    def __init__(self):
-        import os.path
-        sample_seg = '%s/../data/sample.1.seg' % (os.path.dirname(__file__))
-        super(SEGSample, self).__init__(sample_seg, video='sample')
+        # source identifier 1 start duration
+        format = '{VIDEO} {ID} {NA} {START} {DURATION}' 
+        super(SEGParser, self).__init__(path2seg, \
+                                         format, \
+                                         sliding_window = None, 
+                                         multitrack = multitrack)
 
 def toSEG(annotation, toFrameRange, delta=0, order_by='time'):
     """
