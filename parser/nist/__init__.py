7e4633af5f089d4a515c9de9bd5135ab324a9d84
Hervé BREDIN <bredin@limsi.fr>
2012-05-09 11:00:44 +0200

    Limit NIST parsers to MDTM and UEM

--- parser/nist/__init__.py
+++ parser/nist/__init__.py
@@ -23,15 +23,10 @@ This module contains parser for common file formats used in NIST Rich Transcript
 See http://www.itl.nist.gov/iad/mig/tests/rt/
 """
 
-# __all__ = ['MDTMParser', 'toMDTM', \
-#            'UEMParser', 'toUEM', \
-#            'ETFParser', 'ETF0Parser']
-__all__ = ['MDTMParser', 'UEMParser', 'ETFParser', 'ETF0Parser']
+__all__ = ['MDTMParser', 'UEMParser']
 
-
-from mdtm import MDTMParser #, toMDTM
-from uem import UEMParser #, toUEM
-from etf import ETFParser, ETF0Parser
+from mdtm import MDTMParser
+from uem import UEMParser
 
 if __name__ == "__main__":
     import doctest
