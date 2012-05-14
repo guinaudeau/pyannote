a90c783fccc66347dd646c9dd0629d905fa0ab64
Hervé BREDIN <bredin@limsi.fr>
2012-05-14 12:33:24 +0200

    UEMParser now inherits from BaseTextualTimelineParser

--- pyannote/parser/nist/uem.py
+++ pyannote/parser/nist/uem.py
@@ -18,54 +18,30 @@
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyannote import Segment, Timeline
+from pyannote.base.segment import Segment
+from pyannote.parser.base import BaseTextualTimelineParser
 
-class UEMParser(object):
-    """
-    .uem file parser
-    """
-    def __init__(self, path2uem, \
-                       ):
-        
+class UEMParser(BaseTextualTimelineParser):
+    
+    def __init__(self):
         super(UEMParser, self).__init__()
-        self.path2uem = path2uem
+    
+    def _comment(self, line):
+        return line[:2] == ';;'
+    
+    def _parse(self, line):
         
-        # empty list of uems
-        self.uems = {}
+        tokens = line.split()
+        # video channel start end
         
-        # parse file
-        f = open(self.path2uem, 'r')
-        for line in f:
-            # skip comments
-            if line[:2] == ';;':
-                continue
-            # split line into fields
-            fields = line.strip().split()
-            video = fields[0]
-            channel = int(fields[1])
-            start = float(fields[2])
-            end   = float(fields[3])
-            
-            if video not in self.uems:
-                self.uems[video] = Timeline(video=video)
-            
-            self.uems[video] += Segment(start, end)
-    
-    def timeline(self, video):
-        return self.uems[video]
-    
-    def videos(self):
-        return self.uems.keys()
-    
-def toUEM(timeline):
-    """"""
-    video = timeline.video
-    text = ''
-    for s, segment in enumerate(timeline):
-        start = segment.start
-        end = segment.end
-        text += '%s 1 %g %g\n' % (video, start, end)
-    return text
+        video = str(tokens[0])
+        #channel = tokens[1]
+        start_time = float(tokens[2])
+        end_time = float(tokens[3])
+        segment = Segment(start=start_time, end=end_time)
+        print segment
+        
+        return segment, video
 
 if __name__ == "__main__":
     import doctest
