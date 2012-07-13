e51e061659a0e957a9307f4b537bb56ddc190896
Hervé BREDIN <bredin@limsi.fr>
2012-07-13 18:53:51 +0200

    BaseModelMixin documentation

--- pyannote/algorithm/clustering/model/base.py
+++ pyannote/algorithm/clustering/model/base.py
@@ -19,24 +19,81 @@
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
 class BaseModelMixin(object):
+    """
+    Clustering model mixin
     
+    """
     def mmx_setup(self, **kwargs):
+        """
+        Setup model internal variables
+        """
         pass
     
-    def mmx_symmetric(self):
-        return False
-    
-    def mmx_fit(self, label):
+    def mmx_fit(self, label, **kwargs):
+        """
+        Create model
+        
+        Parameters
+        ----------
+        label : any valid label
+            The `label` to model
+        
+        Returns
+        -------
+        model : any object
+            The model for `label`
+        
+        """
         name = self.__class__.__name__
         raise NotImplementedError('%s sub-class must implement method'
                                   'mmx_fit()' % name)
     
-    def mmx_compare(self, label, other_label):
+    def mmx_symmetric(self):
+        """
+        Is model similarity symmetric?
+        
+        Returns
+        -------
+        symmetric: bool
+            True if similarity is symmetric, False otherwise.
+        
+        """
+        return False
+    
+    def mmx_compare(self, label, other_label, **kwargs):
+        """
+        Similarity between two labels
+        
+        Parameters
+        ----------
+        label, other_label : any valid label
+            The labels to compare
+            
+        Returns
+        -------
+        similarity : float
+            Similarity between the two labels, the higher the more similar
+        
+        """
         name = self.__class__.__name__
         raise NotImplementedError('%s sub-class must implement method'
                                   'mmx_compare()' % name)
     
-    def mmx_merge(self, labels):
+    def mmx_merge(self, labels, **kwargs):
+        """
+        Merge models
+        
+        Parameters
+        ----------
+        labels : list of valid labels
+            The labels whose models should be merged
+            
+        Returns
+        -------
+        model : any object
+            The merged models
+        
+        """
         name = self.__class__.__name__
         raise NotImplementedError('%s sub-class must implement method'
                                   'mmx_merge()' % name)
