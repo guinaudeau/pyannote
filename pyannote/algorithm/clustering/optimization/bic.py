4e595756af0e1aeae16e793be6c837304f278db0
Hervé BREDIN <bredin@limsi.fr>
2012-07-17 16:37:20 +0200

    Typo

--- pyannote/algorithm/clustering/optimization/bic.py
+++ pyannote/algorithm/clustering/optimization/bic.py
@@ -1,6 +1,7 @@
 from pyannote.algorithm.clustering.optimization.ilp import IntegerLinearProgramming
 from pyannote.algorithm.clustering.model.gaussian import BICMMx
 
-class BICIPL(IntegerLinearProgramming, BICMMx):
+class BICILP(IntegerLinearProgramming, BICMMx):
     def __init__(self, penalty_coef=3.5):
-        super(BICIPL, self).__init__(penalty_coef=penalty_coef)
+        super(BICILP, self).__init__(penalty_coef=penalty_coef)
+
