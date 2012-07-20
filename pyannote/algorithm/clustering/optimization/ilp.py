4f007155b46b9552fbf50330975ffc3afac57387
Hervé BREDIN <bredin@limsi.fr>
2012-07-20 14:48:34 +0200

    Externalize Gurobi model

--- pyannote/algorithm/clustering/optimization/ilp.py
+++ pyannote/algorithm/clustering/optimization/ilp.py
@@ -3,6 +3,48 @@ import numpy as np
 import networkx as nx
 import pyfusion.normalization.bayes
 
+
+def io_log_prob(P, alpha):
+    N, N = P.shape
+    
+    # Gurobi model
+    model = grb.Model("io_log_prob")
+    
+    x = {}
+    for i in range(N):
+        for j in range(N):
+            name = "x_%d_%d" % (i,j)
+            x[i, j] = model.addVar(vtype=grb.GRB.BINARY, name=name)
+    
+    model.update()
+    
+    # objective
+    h1 = np.maximum(-1e10, np.log(P))
+    h0 = np.maximum(-1e10, np.log(1 - P))
+    
+    objective = grb.quicksum([alpha*h1[i,j]*x[i,j]+h0[i,j]*(1-x[i,j])
+                           for i in range(N) for j in range(i+1, N)])
+    model.setObjective(objective, grb.GRB.MAXIMIZE)
+    
+    # symmetry constraint
+    s = {}
+    for i in range(N):
+        for j in range(N):
+            s[i,j] = (x[i,j] == x[j,i])
+            name = "s_%d_%d" % (i,j)
+            model.addConstr(s[i,j], name)
+    
+    # transitivity constraints
+    t = {}
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                t[i,j,k] = (1-x[i,j])+(1-x[j,k]) >= (1-x[i,k])
+                name = "t_%d_%d_%d" % (i, j, k)
+                model.addConstr(t[i,j,k], name)
+    
+    return x, model
+    
 class IntegerLinearProgramming(object):
     
     def __init__(self, **kwargs):
@@ -94,67 +136,6 @@ class IntegerLinearProgramming(object):
                                                                 neg_label=0)
         self.posterior.fit(X, y=y)
     
-    def _get_ilp(self, P, alpha=None):
-        """
-        
-        Parameters
-        ----------
-        P : array-like (num_tracks, num_tracks)
-            P[i, j] is the probability for i & j to be in the same cluster
-            (0 <= P[i, j] <= 1 for all i & j)
-        alpha : float, optional
-            Defaults to 1./num_tracks
-            alpha = 1. means one cluster per track (higher purity)
-            alpha = 0. means one big cluster (higher coverage)
-        """
-        N, N = P.shape
-        
-        if alpha is None:
-            alpha = 1./N
-        
-        # Gurobi model
-        model = grb.Model("ipl")
-        
-        # model variables
-        # xij = 1 <==> i & j in the same cluster
-        x = {}
-        for i in range(N):
-            for j in range(N):
-                name = "x_%d_%d" % (i,j)
-                x[i, j] = model.addVar(vtype=grb.GRB.BINARY, name=name)
-        
-        model.update()
-        
-        # objective
-        # maximize intra-cluster probability 
-        # minimize inter-cluster probability
-        h1 = np.maximum(-1e10, np.log(P))
-        h0 = np.maximum(-1e10, np.log(1 - P))
-        
-        objective = grb.quicksum([alpha*h1[i,j]*x[i,j]+h0[i,j]*(1-x[i,j])
-                               for i in range(N) for j in range(i+1, N)])
-        model.setObjective(objective, grb.GRB.MAXIMIZE)
-        
-        # symmetry constraint
-        s = {}
-        for i in range(N):
-            for j in range(N):
-                s[i,j] = (x[i,j] == x[j,i])
-                name = "s_%d_%d" % (i,j)
-                model.addConstr(s[i,j], name)
-        
-        # transitivity constraints
-        t = {}
-        for i in range(N):
-            for j in range(N):
-                for k in range(N):
-                    t[i,j,k] = (1-x[i,j])+(1-x[j,k]) >= (1-x[i,k])
-                    name = "t_%d_%d_%d" % (i, j, k)
-                    model.addConstr(t[i,j,k], name)
-        
-        # return x, m
-        return x, model
-        
     def __call__(self, annotation, feature, alpha=None):
         
         # get tracks similarity & make it a posterior probability P
@@ -163,7 +144,7 @@ class IntegerLinearProgramming(object):
         P = self.posterior.transform(X.reshape((-1, 1))).reshape((N, N))
         
         # optimization
-        x, model = self._get_ilp(P, alpha=alpha)
+        x, model = io_log_prob(P, alpha)
         model.setParam('OutputFlag', False)
         model.optimize()
         
