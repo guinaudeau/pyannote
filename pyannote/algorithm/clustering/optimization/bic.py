a44cbcd3756b02cafd68f506ba421205df7bb37d
Hervé BREDIN <bredin@limsi.fr>
2012-07-25 15:17:36 +0200

    Working (yeaaah) ILP BIC clustering

--- pyannote/algorithm/clustering/optimization/bic.py
+++ pyannote/algorithm/clustering/optimization/bic.py
@@ -1,7 +1,10 @@
-from pyannote.algorithm.clustering.optimization.ilp import IntegerLinearProgramming
+#!/usr/bin/env python
+# encoding: utf-8
+
+from pyannote.algorithm.clustering.optimization.base import ILPClustering
 from pyannote.algorithm.clustering.model.gaussian import BICMMx
 
-class BICILP(IntegerLinearProgramming, BICMMx):
-    def __init__(self, penalty_coef=3.5):
-        super(BICILP, self).__init__(penalty_coef=penalty_coef)
-
+class BICILP(ILPClustering, BICMMx):
+    def __init__(self, alpha=1e-4, penalty_coef=3.5):
+        super(BICILP, self).__init__(alpha=alpha, penalty_coef=penalty_coef)
+        
