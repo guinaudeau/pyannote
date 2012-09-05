b24eb3d14b36db45151be89e3cb5061cef14d44b
Hervé BREDIN <bredin@limsi.fr>
2012-09-05 14:02:30 +0200

    New option --no-overlap and better comments in code

--- bin/der.py
+++ bin/der.py
@@ -23,7 +23,7 @@ from argparse import ArgumentParser
 
 argparser = ArgumentParser(description='A tool for evaluation of (speaker) diarization')
 argparser.add_argument('--version', action='version', 
-                    version=('PyAnnote %s' % pyannote.__version__))
+                       version=('PyAnnote %s' % pyannote.__version__))
 
 from pyannote.parser import AnnotationParser, TimelineParser
 def groundtruth_parser(path):
@@ -33,12 +33,22 @@ def hypothesis_parser(path):
 def uem_parser(path):
     return TimelineParser().read(path)
 
+# First positional argument is groundtruth file
+# It is loaded at argument-parsing time by an instance of AnnotationParser
 argparser.add_argument('groundtruth', type=groundtruth_parser,
                        help='path to groundtruth diarization')
+
+# Next positional arguments (at least one) are hypothesis files
+# They are loaded at argument-parsing time by an instance of AnnotationParser
 argparser.add_argument('hypothesis', nargs='+', type=hypothesis_parser,
                         help='path to automatic diarization')
+
+# UEM file is loaded at argument-parsing time by an instance of TimelineParser
 argparser.add_argument('--uem', type=uem_parser, 
                        help='path to Unpartitioned Evaluation Map (UEM)')
+
+# Various switches to compute purity, coverage, homogeneity or completeness
+# (diarization error rate is always computed)
 argparser.add_argument('--purity', action='store_true',
                        help='compute diarization purity')
 argparser.add_argument('--coverage', action='store_true',
@@ -47,76 +57,135 @@ argparser.add_argument('--homogeneity', action='store_true',
                        help='compute diarization homogeneity')
 argparser.add_argument('--completeness', action='store_true',
                        help='compute diarization completeness')
+
+# When provided, removes overlapping speech regions from evaluation
+argparser.add_argument('--no-overlap', action='store_true',
+                       help='remove overlapping speech regions from evaluation')
+
+# Actual argument parsing
 args = argparser.parse_args()
 
-
-from prettytable import PrettyTable
+# initialize header of results table
 header = ['', 'DER']
 
+# initialize DER metric
 import pyannote.metric
 der = [pyannote.metric.DiarizationErrorRate() 
        for _ in args.hypothesis]
 
+# initialize purity metric & update table header
 if args.purity:
     header.append('Purity')
     purity = [pyannote.metric.diarization.DiarizationPurity() 
               for _ in args.hypothesis]
+
+# initialize coverage metric & update table header
 if args.coverage:
     header.append('Coverage')
     coverage = [pyannote.metric.diarization.DiarizationCoverage() 
                 for _ in args.hypothesis]
+
+# initialize homogeneity metric & update table header
 if args.homogeneity:
     header.append('Homogeneity')
     homogeneity = [pyannote.metric.diarization.DiarizationHomogeneity() 
                    for _ in args.hypothesis]
+
+# initialize completeness metric & update table header
 if args.completeness:
     header.append('Completeness')
     completeness = [pyannote.metric.diarization.DiarizationCompleteness() 
                     for _ in args.hypothesis]
 
+# initialize results table
+from prettytable import PrettyTable
 table = PrettyTable(header)
 table.float_format = '1.3'
 table.align[''] = 'l'
 
+# process each URI, one after the other
 for uri in args.groundtruth.videos:
     
+    # read reference for current URI
     ref = args.groundtruth(uri)
     
+    # read UEM if provided
     if args.uem is None:
         uem = None
     else:
         uem = args.uem(uri)
     
-    if uem is not None:
-        ref = ref(uem, mode='intersection')
+    # get overlapping speech regions if requested
+    if args.no_overlap:
+        # make sure timeline is a segmentation
+        # tag each resulting segment by all intersecting labels
+        tmp_ref = ref >> (ref._timeline.segmentation())
+        # overlapping speech regions
+        # (ie. timeline made of segments with two tracks or more)
+        overlap = pyannote.Timeline([segment for segment in tmp_ref 
+                                             if len(tmp_ref[segment, :]) > 1])
     
+    # focus on UEM if provided
+    if uem is not None:
+        # update UEM if overlapping speech regions are removed from evaluation
+        # remove overlapping speech regions from UEM if requested
+        if args.no_overlap:
+            uem = overlap.gaps(focus=uem)
+        ref = ref(uem, mode='intersection')
+    else:
+        # remove overlapping speech regions if requested
+        if args.no_overlap:
+            ref = ref(overlap.gaps(focus=ref.coverage()), 
+                                   mode='intersection')
+    
+    # process each hypothesis file, one after the other
     for h, (path, hypothesis) in enumerate(args.hypothesis):
         
+        # add URI to results table
         if len(args.hypothesis) > 1:
             row = ['%s [%d]' % (uri, h+1) ]
         else:
             row = ['%s' % uri]
-            
-        hyp = hypothesis(uri)
-        if uem is not None:
-            hyp = hyp(uem, mode='intersection')
         
+        # read hypothesis for current URI
+        hyp = hypothesis(uri)
+        
+        # focus on UEM if provided
+        if uem is not None:
+            # UEM was already updated to take overlapping speech regions
+            # into account -- so no need to worry about that here.
+            hyp = hyp(uem, mode='intersection')
+        else:
+            # remove overlapping speech regions if requested
+            if args.no_overlap:
+                hyp = hyp(overlap.gaps(focus=hyp.coverage()), 
+                                       mode='intersection')
+        
+        # compute DER
         D = der[h](ref, hyp)
         row.append(D)
         
+        # compute purity
         if args.purity:
             P = purity[h](ref, hyp)
             row.append(P)
+        
+        # compute coverage
         if args.coverage:
             C = coverage[h](ref, hyp)
             row.append(C)
+            
+        # compute homogeneity
         if args.homogeneity:
             H = homogeneity[h](ref, hyp)
             row.append(H)
+            
+        # compute completeness
         if args.completeness:
             K = completeness[h](ref, hyp)
             row.append(K)
         
+        # update results table
         table.add_row(row)
 
 print table
@@ -125,6 +194,7 @@ if len(args.hypothesis) > 1:
         print '[%d] %s' % (h+1, path)
     print
 
+# Global errors table
 table = PrettyTable(header)
 table.align[''] = 'l'
 table.float_format = '1.3'
@@ -149,6 +219,7 @@ for h, (path, _) in enumerate(args.hypothesis):
 
 print table
 
+# Confidence interval table
 table = PrettyTable(header)
 table.align[''] = 'l'
 
