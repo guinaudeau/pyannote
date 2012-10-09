017d63874caaa35c1a322e9d636ad6502ced697f
Hervé BREDIN <bredin@limsi.fr>
2012-10-09 19:21:57 +0200

    Slightly optimized transitivity constraints

--- pyannote/algorithm/clustering/optimization/gurobi.py
+++ pyannote/algorithm/clustering/optimization/gurobi.py
@@ -7,115 +7,15 @@ import networkx as nx
 import gurobipy as grb
 from progressbar import ProgressBar, Percentage, Bar, ETA
 
-# import pyfusion.normalization.bayes
+def _n01(g, n1, n2):
+    if g.has_edge(n1, n2):
+        p = g[n1][n2]['probability']
+        if p in [0,1]:
+            return int(p)
+    return None
 
-# def new_clustering_model(N, problem_name=None):
-#     """
-#     Create and return Gurobi clustering model
-#     
-#     The model contains the following variables and constraints:
-#         * NxN binary variables x[i,j] indicating whether items i and j
-#           are in the same cluster (denoted i-j, the opposite being i|j)
-#         * NxN symmetry constraints
-#                      i-j ==> j-i
-#                      i|j ==> j|i
-#         * NxNxN transitity constraints
-#                      i-j and j-k ==> i-k
-#                      i-j and j|k ==> i|k
-#     
-#     Note that returned Gurobi model has no set objective.
-#     
-#     Parameters
-#     ----------
-#     N : int
-#         Size of the model (number of clustered items).
-#     name : str, optional
-#         Name of the model
-#     
-#     Returns
-#     -------
-#     model : :class:`gurobipy.Model`
-#         Gurobi clustering model with no objective.
-#     x : dict
-#         Gurobi clustering variable x[i,j]
-#     """
-#     
-#     # create empty model
-#     model = grb.Model(problem_name)
-#     
-#     # add (to-be-optimized) variables
-#     # xij = 1 means i-j (i & j in the same cluster)
-#     x = {}
-#     for i in range(N):
-#         for j in range(N):
-#             name = "x_%d_%d" % (i,j)
-#             x[i, j] = model.addVar(vtype=grb.GRB.BINARY, name=name)
-#     
-#     # update model with new (to-be-optimized) variables
-#     model.update()
-#     
-#     # add symmetry constraint
-#     # i-j ==> j-i
-#     s = {}
-#     for i in range(N):
-#         for j in range(N):
-#             s[i,j] = (x[i,j] == x[j,i])
-#             name = "s_%d_%d" % (i,j)
-#             model.addConstr(s[i,j], name)
-#     
-#     # add transitivity constraints
-#     # i-j and j-k ==> i-k
-#     # (also implies i-j and j|k ==> i|k)
-#     t = {}
-#     for i in range(N):
-#         for j in range(N):
-#             for k in range(N):
-#                 t[i,j,k] = (1-x[i,j])+(1-x[j,k]) >= (1-x[i,k])
-#                 name = "t_%d_%d_%d" % (i, j, k)
-#                 model.addConstr(t[i,j,k], name)
-#     
-#     # return the model & its variables
-#     return model, x
-
-
-# def optimize(N, model, x):
-#     """
-#     Optimize model and return clusters
-#     
-#     Parameters
-#     ----------
-#     N : int
-#         Size of the model (number of clustered items).
-#     model : :class:`gurobipy.Model`
-#         Gurobi clustering model
-#     x : dict
-#         Gurobi clustering variable x[i,j]
-#     
-#     Returns
-#     -------
-#     clusters : list of lists
-#     
-#     """
-#     
-#     model.optimize()
-#     
-#     # read results as a graph
-#     # one node per label, edges between same-cluster labels
-#     g = nx.Graph()
-#     for i in range(N):
-#         g.add_node(i)
-#         for j in range(N):
-#             if j <= i:
-#                 continue
-#             value = x[i,j].x
-#             if value:
-#                 g.add_edge(i, j)
-#         
-#     # find clusters (connected components in graph)
-#     clusters = nx.connected_components(g)
-#     
-#     return clusters
-
+def _ij2nn(ij, i2n):
+    return i2n[ij[0]], i2n[ij[1]]
 
 def graph2gurobi(g):
     """
@@ -164,81 +64,106 @@ def graph2gurobi(g):
             n2 = nodes[i2]
             x[n1, n2] = model.addVar(vtype=grb.GRB.BINARY)
     
-    # for n, node in enumerate(nodes):
-    #     pb.update(n+1)
-    #     for other_node in nodes:
-    #         x[node, other_node] = model.addVar(vtype=grb.GRB.BINARY)
     pb.finish()
     
     model.update()
     
-    # # symmetry constraints
-    # pb.widgets[0] = 'Symmetry constraints'
-    # pb.maxval = N
-    # pb.start()
-    # for n, node in enumerate(nodes):
-    #     pb.update(n+1)
-    #     for other_node in nodes:
-    #         model.addConstr(x[node, other_node] == x[other_node, node])
-    # pb.finish()
-    
-    # trivial (0/1) probability constraints
-    pb.widgets[0] = 'Hard constraints'
-    # edges = [(e, f, d) for (e, f, d) in g.edges(data=True) 
-    #                    if d['probability'] in [0, 1]]
-    
-    # Σi|1..N ( Σj|i+1..N 1 )
-    pb.maxval = int(N*(N-1)/2.)
-    pb.start()
-    for i1 in range(N):
-        # Σi|1..i1 ( Σj|i+1..N 1 )
-        n = int(N*i1 - i1**2/2. - i1/2.)
-        pb.update(n)
-        n1 = nodes[i1]
-        for i2 in range(i1+1, N):
-            n2 = nodes[i2]
-            if not g.has_edge(n1, n2):
-                continue
-            probability = g[n1][n2]['probability']
-            if probability == 1:
-                # these 2 nodes must be in the same cluster
-                model.addConstr(x[n1, n2] == 1)
-            elif probability == 0:
-                # these 2 nodes must be in 2 different clusters
-                model.addConstr(x[n1, n2] == 0)
-    
-    # for n, (node, other_node, data) in enumerate(edges):
-    #     pb.update(n+1)
-    #     
-    #     probability = data['probability']
-    #     
-    #     if probability == 1:
-    #         model.addConstr(x[node, other_node] == 1)
-    #     
-    #     elif probability == 0:
-    #         model.addConstr(x[node, other_node] == 0)
-    
-    pb.finish()
-    
     # transitivity constraints
-    pb.widgets[0] = 'Transitivity constraints'
+    pb.widgets[0] = 'Constraints'
     
     # Σi|1..N ( Σj|i+1..N ( Σk|j+1..N 1 ) ) 
     pb.maxval = int(N**3/6. - N**2/2. + N/3.)
     pb.start()
+    
+    p = {}
+    i2n = {}
     for i1 in range(N):
+        
         # Σi|1..i1 ( Σj|i+1..N ( Σk|j+1..N 1 ) ) 
         n = int(N**2*i1/2.- N*i1/2.-N*(i1**2/2.+i1/2.)+i1**3/6.+i1**2/2.+i1/3.)
         pb.update(n)
         
         n1 = nodes[i1]
+        i2n[1] = n1
+        
         for i2 in range(i1+1, N):
+            
             n2 = nodes[i2]
+            i2n[2] = n2
+            
+            # probability n1 <--> n2
+            p[1,2] = _n01(g, n1, n2)
+            
+            if p[1,2] in [0, 1]:
+                model.addConstr(x[n1, n2] == p[1,2])
+            
             for i3 in range(i2+1, N):
+                
                 n3 = nodes[i3]
-                model.addConstr(x[n2, n3] + x[n1, n3] - x[n1, n2] <= 1)
-                model.addConstr(x[n1, n2] + x[n1, n3] - x[n2, n3] <= 1)
-                model.addConstr(x[n1, n2] + x[n2, n3] - x[n1, n3] <= 1)
+                i2n[3] = n3
+                
+                # probability n1 <--> n3
+                p[1,3] = _n01(g, n1, n3)
+                
+                # probability n2 <--> n3
+                p[2,3] = _n01(g, n2, n3)
+                
+                # set of values taken by the 3 edges
+                # {0}, {1}, {None}, {0, 1}, {0, None}, {1, None} or {0, 1, None}
+                values_set = set(p.values())
+                
+                if not (values_set - set([0, 1])):
+                    # there are only 0s and 1s
+                    # those constraints will be taken care of by the outer loop
+                    continue
+                
+                # {0: number of 0s, 1: number of 1s, None: number of Nones}
+                value2count = {v: p.values().count(v) for v in [0, 1, None]}
+                value2list = {v: [ij for ij in p if p[ij] == v] 
+                              for v in [0, 1, None]}
+                
+                # 0/1 values
+                values_set = values_set - set([None])
+                num_values = len(values_set)
+                
+                # there is only one None
+                if value2count[None] == 1:
+                    
+                    ij_none = value2list[None][0]
+                    ninj_none = _ij2nn(ij_none, i2n)
+                    if num_values == 1:
+                        # there are one None and two 0s (or two 1s)
+                        # set the one None to 0 (or to 1)
+                        model.addConstr(x[ninj_none] == values_set.pop())
+                    else:
+                        # there are one None, one 0 and one 1
+                        # set the one None to 0
+                        model.addConstr(x[ninj_none] == 0)
+                
+                # there are two Nones
+                elif value2count[None] == 2:
+                    
+                    # 
+                    ij = value2list[None][0]
+                    ninj = _ij2nn(ij, i2n)
+                    jk = value2list[None][1]
+                    njnk = _ij2nn(jk, i2n)
+                    
+                    if value2count[1] == 1:
+                        # there are two Nones and one 1
+                        # set the two Nones equal to each other
+                        model.addConstr(x[ninj] == x[njnk])
+                    else:
+                        # there two Nones and one 0
+                        # set the two Nones different from each other
+                        model.addConstr(x[ninj] + x[njnk] <= 1)
+                
+                # there are three Nones
+                else:
+                    model.addConstr(x[n2, n3] + x[n1, n3] - x[n1, n2] <= 1)
+                    model.addConstr(x[n1, n2] + x[n1, n3] - x[n2, n3] <= 1)
+                    model.addConstr(x[n1, n2] + x[n2, n3] - x[n1, n3] <= 1)
+                
     
     pb.finish()
     
