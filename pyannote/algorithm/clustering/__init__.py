4068bad1270ebcd40e32b2ba951da5d665aeff99
Hervé BREDIN <bredin@limsi.fr>
2012-10-09 09:08:17 +0200

    BICClustering & BICRecombiner are now imported with pyannote.algorithm.clustering.*

--- pyannote/algorithm/clustering/__init__.py
+++ pyannote/algorithm/clustering/__init__.py
@@ -18,6 +18,10 @@
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['BICClustering']
+__all__ = ['BICClustering', 'BICRecombiner']
 
-from agglomerative import BICClustering
+from agglomerative.bic import BICClustering, BICRecombiner
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
