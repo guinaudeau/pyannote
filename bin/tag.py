cb7fa4ece70e495701f5386517a2215868d34d62
Hervé BREDIN <bredin@limsi.fr>
2012-10-23 14:50:15 +0200

    Basic tag.py command line tool for name propagation

---
+++ bin/tag.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright 2012 Herve BREDIN (bredin@limsi.fr)
+
+# This file is part of PyAnnote.
+# 
+#     PyAnnote is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     PyAnnote is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import pyannote
+import numpy as np
+from argparse import ArgumentParser, SUPPRESS
+from progressbar import ProgressBar, Bar, ETA
+
+from pyannote.parser import AnnotationParser, TimelineParser
+from pyannote.parser import LSTParser, MDTMParser
+
+from pyannote.base.matrix import Cooccurrence
+from pyannote.algorithm.tagging import HungarianTagger, ArgMaxTagger
+
+argparser = ArgumentParser(description='A tool for tagging of annotations')
+argparser.add_argument('--version', action='version', 
+                       version=('PyAnnote %s' % pyannote.__version__))
+
+def annotation_parser(path):
+    return AnnotationParser().read(path)
+
+def uem_parser(path):
+    return TimelineParser().read(path)
+
+def uris_parser(path):
+    return LSTParser().read(path)
+
+def output_parser(path):
+    return open(path, 'w')
+
+argparser.add_argument('input', type=annotation_parser,
+                       help='path to input annotation')
+
+argparser.add_argument('names', type=annotation_parser,
+                        help='path to name annotation')
+
+argparser.add_argument('output', type=output_parser,
+                        help='path to output')
+
+argparser.add_argument('--uris', type=uris_parser, default=SUPPRESS,
+                       help='list of resources to process')
+
+argparser.add_argument('--uem', type=uem_parser, default=SUPPRESS,
+                       help='only process provided time ranges')
+
+mgroup = argparser.add_mutually_exclusive_group()
+
+mgroup.add_argument('--one-to-one', action='store_true',
+                    help='perform one name-to-one input label mapping')
+
+mgroup.add_argument('--one-to-many', action='store_true',
+                    help='perform one name-to-many input labels mapping')
+
+
+# Actual argument parsing
+args = argparser.parse_args()
+
+# List of requested metrics
+if args.one_to_one:
+    tagger = HungarianTagger()
+
+if args.one_to_many:
+    tagger = ArgMaxTagger()
+
+# only evaluate selection of uris
+if hasattr(args, 'uris'):
+    uris = args.uris
+else:
+    uris = args.input.videos
+
+# process each URI, one after the other
+for u, uri in enumerate(uris):
+    
+    # read input for current URI
+    original = args.input(uri).anonymize()
+    
+    # read names for current URI
+    names = args.names(uri)
+    
+    # focus on UEM if provided
+    if hasattr(args, 'uem'):
+        uem = args.uem(uri)
+        names = names(uem, mode='intersection')
+    
+    tagged = tagger(names, original)
+    
+    MDTMParser().write(tagged, f=args.output)
+
+args.output.close()
+    
