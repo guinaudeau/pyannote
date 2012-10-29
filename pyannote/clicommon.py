be5d2ddbad1d17903fa9a4697a297538870705b4
Hervé BREDIN <bredin@limsi.fr>
2012-10-29 13:14:13 +0100

    New function clicommon.containsURI

--- pyannote/clicommon.py
+++ pyannote/clicommon.py
@@ -7,9 +7,6 @@ parser = ArgumentParser(add_help=False)
 parser.add_argument('--version', action='version', 
                        version=('PyAnnote %s' % pyannote.__version__))
 
-# parser.add_argument('--verbose', action='store_true',
-#                     help='print progress information')
-
 parser.add_argument('--verbose', action='append_const', dest='verbose',
                     const=1, default=[], help='print progress information')
 
@@ -26,9 +23,44 @@ parser.add_argument('--uem', type=uem_parser, metavar='file.uem',
                        help='part of resources to process')
 
 URIS = ["%s", "[URI]"]
+"""List of allowed URI place-holders"""
+
 def replaceURI(path, uri):
+    """
+    Replace URI place-holder
+    
+    Parameters
+    ----------
+    path : str
+        Path with URI place-holder
+    uri : str
+        URI
+    
+    Returns
+    -------
+    new_path : str
+        `path` with URI place-holder replaced by `uri`
+    
+    """
     new_path = str(path)
     for ph in URIS:
         new_path = new_path.replace(ph, uri)
     return new_path
 
+def containsURI(path):
+    """
+    Check if path contains URI place-holder
+    
+    Parameters
+    ----------
+    path : str
+        Path
+        
+    Returns
+    -------
+    contains : bool
+        True if `path` contains one (or more) URI place-holders
+        False otherwise
+    
+    """
+    return any([path.find(ph) > -1 for ph in URIS])
