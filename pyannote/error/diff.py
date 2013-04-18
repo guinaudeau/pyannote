320c17b98344f4089185d38cb6552a3bd4acf558
Hervé BREDIN <bredin@limsi.fr>
2013-04-18 19:12:47 +0200

    Error analysis update! Regression test!

--- pyannote/error/diff.py
+++ pyannote/error/diff.py
@@ -18,149 +18,158 @@
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
-import pandas
-
-CONFUSION_NAME = 'Confusion'
-FALSE_ALARM_NAME = 'False alarm'
-MISSED_DETECTION_NAME = 'Missed'
-
-
-class ConfusionError(object):
-
-    def __init__(self, reference, hypothesis):
-        super(ConfusionError, self).__init__()
-        self.__reference = reference
-        self.__hypothesis = hypothesis
-
-    def __get_reference(self):
-        return self.__reference
-
-    def __get_hypothesis(self):
-        return self.__hypothesis
-    reference = property(fget=__get_reference, doc="Reference")
-    hypothesis = property(fget=__get_hypothesis, doc="Hypothesis")
-
-    def __hash__(self):
-        return hash(self.__reference) + hash(self.__hypothesis)
-
-    def __eq__(self, other):
-        return self.__reference == other.__reference and \
-            self.__hypothesis == other.__hypothesis
-
-    def __str__(self):
-        return "%s vs. %s" % (self.reference, self.hypothesis)
-
-    def type(self):
-        return CONFUSION_NAME
-
-    def __repr__(self):
-        return str(self)
-
-
-class FalseAlarmError(ConfusionError):
-    def __init__(self, hypothesis):
-        super(FalseAlarmError, self).__init__('∅', hypothesis)
-
-    def type(self):
-        return FALSE_ALARM_NAME
-
-
-class MissedDetectionError(ConfusionError):
-    def __init__(self, reference):
-        super(MissedDetectionError, self).__init__(reference, '∅')
-
-    def type(self):
-        return MISSED_DETECTION_NAME
+import numpy as np
+from munkres import Munkres
 
 from pyannote.metric.identification import UnknownIDMatcher
-from pyannote.base.mapping import OneToOneMapping, NoMatch
 from pyannote.base.annotation import Annotation
+
+from pyannote.metric.identification import IER_CORRECT, \
+    IER_CONFUSION, \
+    IER_FALSE_ALARM, \
+    IER_MISS
+
+REGRESSION = 'regression'
+IMPROVEMENT = 'improvement'
+BOTH_CORRECT = 'both_correct'
+BOTH_INCORRECT = 'both_incorrect'
 
 
 class Diff(object):
 
-    def __init__(self, matcher=None):
+    def __init__(self, matcher=None, unknown=True):
+
         super(Diff, self).__init__()
+
         if matcher:
-            self.__matcher = matcher
+            self.matcher = matcher
         else:
-            self.__matcher = UnknownIDMatcher()
+            self.matcher = UnknownIDMatcher()
+        self.unknown = unknown
+        self.munkres = Munkres()
 
-    def __call__(self, reference, hypothesis, df=False):
+    def compare(self, reference, hypothesis, correct=True):
+        """
 
-        timeline = (reference.timeline + hypothesis.timeline).segmentation()
+        Returns
+        -------
+        diff : `Annotation`
+            Annotation containing list of errors ()
 
-        reference = reference >> timeline
-        hypothesis = hypothesis >> timeline
+        """
 
-        inventory = Annotation(uri=reference.uri, modality=reference.modality)
+        # common (up-sampled) timeline
+        common_timeline = reference.timeline + hypothesis.timeline
+        common_timeline = common_timeline.segmentation()
 
-        for segment in timeline:
-            R = reference.get_labels(segment)
-            H = hypothesis.get_labels(segment)
-            matching = self.__diff(R, H)
-            for r, h in matching.to_dict(single=True).iteritems():
-                if isinstance(r, NoMatch):
-                    error = FalseAlarmError(h)
-                elif isinstance(h, NoMatch):
-                    error = MissedDetectionError(r)
+        # align reference on common timeline
+        R = reference >> common_timeline
+
+        # translate and align hypothesis on common timeline
+        H = hypothesis >> common_timeline
+
+        self.diff = Annotation(uri=reference.uri, modality=reference.modality)
+
+        # loop on all segments
+        for segment in common_timeline:
+
+            # # segment duration
+            # duration = segment.duration
+
+            # list of IDs in reference segment
+            rlabels = R.get_labels(segment, unknown=self.unknown, unique=False)
+
+            # list of IDs in hypothesis segment
+            hlabels = H.get_labels(segment, unknown=self.unknown, unique=False)
+
+            _, details = self.matcher.manyToManyMatch(rlabels, hlabels)
+
+            if correct:
+                for r, h in details[IER_CORRECT]:
+                    track = self.diff.new_track(segment, candidate=IER_CORRECT, prefix=IER_CORRECT)
+                    self.diff[segment, track] = (IER_CORRECT, r, h)
+
+            for r, h in details[IER_CONFUSION]:
+                track = self.diff.new_track(segment, candidate=IER_CONFUSION, prefix=IER_CONFUSION)
+                self.diff[segment, track] = (IER_CONFUSION, r, h)
+
+            for r in details[IER_MISS]:
+                track = self.diff.new_track(segment, candidate=IER_MISS, prefix=IER_MISS)
+                self.diff[segment, track] = (IER_MISS, r, None)
+
+            for h in details[IER_FALSE_ALARM]:
+                track = self.diff.new_track(segment, candidate=IER_FALSE_ALARM, prefix=IER_FALSE_ALARM)
+                self.diff[segment, track] = (IER_FALSE_ALARM, None, h)
+
+        return self.diff.copy()
+
+    def _match_errors(self, old_error, new_error):
+        old_type, old_ref, old_hyp = old_error
+        new_type, new_ref, new_hyp = new_error
+        return (old_ref == new_ref) * ((old_type == new_type) + (old_hyp == new_hyp))
+
+    def regression(self, reference, old_hypothesis, new_hypothesis):
+
+        reference = reference.smooth()
+        old_hypothesis = old_hypothesis.smooth()
+        new_hypothesis = new_hypothesis.smooth()
+        old_diff = self.compare(reference, old_hypothesis, correct=True).smooth()
+        new_diff = self.compare(reference, new_hypothesis, correct=True).smooth()
+        common_timeline = (old_diff.timeline + new_diff.timeline).segmentation()
+        old_diff = old_diff >> common_timeline
+        new_diff = new_diff >> common_timeline
+
+        regression = Annotation(uri=reference.uri, modality=reference.modality)
+
+        for segment in common_timeline:
+
+            old_errors = old_diff.get_labels(segment, unique=False)
+            new_errors = new_diff.get_labels(segment, unique=False)
+
+            n1 = len(old_errors)
+            n2 = len(new_errors)
+            n = max(n1, n2)
+
+            match = np.zeros((n, n), dtype=int)
+            for i1, e1 in enumerate(old_errors):
+                for i2, e2 in enumerate(new_errors):
+                    match[i1, i2] = self._match_errors(e1, e2)
+
+            mapping = self.munkres.compute(2-match)
+
+            for i1, i2 in mapping:
+
+                if i1 >= n1:
+                    track = regression.new_track(segment, candidate=REGRESSION, prefix=REGRESSION)
+                    regression[segment, track] = (REGRESSION, None, new_errors[i2])
+
+                elif i2 >= n2:
+                    track = regression.new_track(segment, candidate=IMPROVEMENT, prefix=IMPROVEMENT)
+                    regression[segment, track] = (IMPROVEMENT, old_errors[i1], None)
+
+                elif old_errors[i1][0] == IER_CORRECT:
+
+                    if new_errors[i2][0] == IER_CORRECT:
+                        track = regression.new_track(segment, candidate=BOTH_CORRECT, prefix=BOTH_CORRECT)
+                        regression[segment, track] = (BOTH_CORRECT, old_errors[i1], new_errors[i2])
+
+                    else:
+                        track = regression.new_track(segment, candidate=REGRESSION, prefix=REGRESSION)
+                        regression[segment, track] = (REGRESSION, old_errors[i1], new_errors[i2])
+
                 else:
-                    error = ConfusionError(r, h)
-                track = inventory.new_track(segment,
-                                            candidate=error.type(),
-                                            prefix=error.type())
-                inventory[segment, track] = error
 
-        return inventory
+                    if new_errors[i2][0] == IER_CORRECT:
+                        track = regression.new_track(segment, candidate=IMPROVEMENT, prefix=IMPROVEMENT)
+                        regression[segment, track] = (IMPROVEMENT, old_errors[i1], new_errors[i2])
 
-    def __diff(self, ids1, ids2):
+                    else:
+                        track = regression.new_track(segment, candidate=BOTH_INCORRECT, prefix=BOTH_INCORRECT)
+                        regression[segment, track] = (BOTH_INCORRECT, old_errors[i1], new_errors[i2])
 
-        # duplicate because we are going to modify it.
-        ids1_cp = set(ids1)
-        ids2_cp = set(ids2)
+        return regression.smooth()
 
-        mapping = OneToOneMapping('reference', 'hypothesis')
-        for id1 in ids1:
-            # get list of matching IDs in ids2
-            matched = set([id2 for id2 in ids2 if self.__matcher(id1, id2)])
-            # if we found at least one match
-            if matched:
-                ids1_cp.remove(id1)
-                ids2_cp.remove(matched.pop())
-
-        # confusions
-        for id1 in set(ids1_cp):
-            if ids2_cp:
-                mapping += ([id1], [ids2_cp.pop()])
-                ids1_cp.remove(id1)
-
-        # missed detections
-        for id1 in ids1_cp:
-            mapping += ([id1], None)
-
-        # false alarms
-        for id2 in ids2_cp:
-            mapping += (None, [id2])
-
-        return mapping
-
-    def aggregate(self, inventory, top=None):
-        unordered = [(error, inventory.subset(set([error])).timeline.duration())
-                     for error in inventory.labels()]
-        ordered = sorted(unordered, key=lambda x: x[1], reverse=True)
-        total = sum([d for _, d in ordered])
-        data = {'Type': [e.type() for e, _ in ordered],
-                'Reference': [e.reference for e, _ in ordered],
-                'Hypothesis': [e.hypothesis for e, _ in ordered],
-                'Duration': [d for _, d in ordered],
-                '%': [int(100 * d / total) for _, d in ordered]}
-        df = pandas.DataFrame(data=data, columns=['Type', 'Reference', 'Hypothesis', 'Duration', '%'])
-        if top:
-            df = df[:top]
-        return df
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
-
