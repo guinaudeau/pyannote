64522ea749211ba50ac4b167bfd3b083c4b2d4c6
Hervé BREDIN <bredin@limsi.fr>
2013-05-22 16:17:15 +0200

    gurobi2 work in progress

--- pyannote/algorithm/mpg/gurobi2.py
+++ pyannote/algorithm/mpg/gurobi2.py
@@ -22,6 +22,8 @@
 import sys
 import numpy as np
 import networkx as nx
+from pyannote import Annotation, Unknown
+from node import TrackNode, IdentityNode
 
 # initialize Gurobi solver
 try:
@@ -40,88 +42,34 @@ except:
         sys.stderr.write('Cannot initialize Gurobi solver.')
 
 
-class ILPClusteringMixin(object):
+class ILPClustering(object):
     """
-    MIP problem solving mixin.
-
-    Assumes instance has the following two attributes:
-
-    instance.model : `grb.Model`
-        Full Gurobi MIP problem
-    instance.x : dict
-        Dictionary containing the variables of the MIP problem `model`
-
     """
+    def __init__(self):
+        super(ILPClustering, self).__init__()
 
-    def init_model(self):
-        """"""
-        # create empty Gurobi model (and shut the **** up)
+        # empty (silent) model
         self.model = grb.Model()
         self.model.setParam(grb.GRB.Param.OutputFlag, False)
 
-    def init_variables(self, items):
-        """"""
-
-        # one binary variable per item pair
+        # empty set of variables
         self.x = {}
+
+    # =================================================================
+    # VARIABLES
+    # =================================================================
+
+    def add_pair_variables(self, items):
+        """Add one variable per pair of items"""
 
         for I in items:
             for J in items:
                 self.x[I, J] = self.model.addVar(vtype=grb.GRB.BINARY)
-
         self.model.update()
 
-    def update_model(self):
-        """"""
-
-        self.model.update()
-
-    def __init__(self, items, similarity, get_similarity=None,
-                 debug=False, **kwargs):
-        """
-        Create MIP clustering problem
-
-        Parameters
-        ----------
-        items : hashables iterable
-
-        similarity :
-
-        get_similarity : func, optional
-            Takes three arguments item1, item2 and similarity
-            and returns the similarity between item1 and item2
-            or NaN when the similarity is not available.
-
-        """
-
-        super(ILPClusteringMixin, self).__init__()
-
-        # create empty Gurobi model
-        self.init_model()
-
-        # add variables
-        self.init_variables(items)
-
-        # Gurobi updates model lazily
-        self.update_model()
-
-        # Default assumes `similarity` is a pandas `DataFrame`
-        if not get_similarity:
-            get_similarity = lambda i, j, S: S[i][j]
-
-        # set_constraints method is not provided by this base class
-        # it must be provided by a ConstraintMixin
-        if debug:
-            sys.stderr.write("DEBUG: set constraints\n")
-        self.set_constraints(items, similarity, get_similarity,
-                             debug=debug, **kwargs)
-
-        # set_objective method is not provided by this base class
-        # it must be provided by a ObjectiveMixin
-        if debug:
-            sys.stderr.write("DEBUG: set objective\n")
-        self.set_objective(items, similarity, get_similarity,
-                           debug=debug, **kwargs)
+    # =================================================================
+    # CONSTRAINTS
+    # =================================================================
 
     def add_reflexivity_constraints(self, items):
         """Add reflexivity constraints (I~I, for all I)"""
@@ -129,23 +77,6 @@ class ILPClusteringMixin(object):
         for I in items:
             constr = self.x[I, I] == 1
             self.model.addConstr(constr)
-
-    def add_hard_constraints(self, items, similarity, get_similarity):
-        """Add hard constraints
-
-        If sim(I, J) = 0, then I|J.
-        If sim(I, J) = 1, then I~J.
-        """
-
-        N = len(items)
-        for i in range(N):
-            I = items[i]
-            for j in range(i+1, N):
-                J = items[j]
-                s = get_similarity(I, J, similarity)
-                if s in [0, 1]:
-                    constr = self.x[I, J] == s
-                    self.model.addConstr(constr)
 
     def add_symmetry_constraints(self, items):
         """Add symmetry constratins
@@ -204,11 +135,42 @@ class ILPClusteringMixin(object):
                     constr = self.x[S, I]+self.x[T, S]-self.x[T, I] <= 1
                     self.model.addConstr(constr)
 
+    def add_hard_constraints(self, items, similarity, get_similarity):
+        """Add hard constraints
+
+        If sim(I, J) = 0, then I|J.
+        If sim(I, J) = 1, then I~J.
+        """
+
+        N = len(items)
+        for i in range(N):
+            I = items[i]
+            for j in range(i+1, N):
+                J = items[j]
+                s = get_similarity(I, J, similarity)
+                if s in [0, 1]:
+                    constr = self.x[I, J] == s
+                    self.model.addConstr(constr)
+
+    def add_unique_identity_constraints(self, tracks, identities):
+        """Add unique identity constraints
+
+        For any track T, T is connected to at most one identity I
+        """
+
+        for T in tracks:
+            constr = grb.quicksum([self.x[T, I] for I in identities]) <= 1
+            self.model.addConstr(constr)
+
+    # =================================================================
+    # OBJECTIVE FUNCTIONS
+    # =================================================================
+
     def get_inter_cluster_dissimilarity(self, items,
                                         similarity, get_similarity):
         """Inter-cluster dissimilarity:  ∑ (1-xij).(1-pij)
-                                        i,j
-
+                                        i∈I
+                                        j∈I
         """
         values = [(1-get_similarity(I, J, similarity))*(1-self.x[I, J])
                   for I in items for J in items
@@ -218,33 +180,34 @@ class ILPClusteringMixin(object):
     def get_intra_cluster_similarity(self, items,
                                      similarity, get_similarity):
         """Intra-cluster similarity: ∑  xij.pij
-                                    i,j
+                                    i∈I
+                                    j∈I
         """
         values = [get_similarity(I, J, similarity)*self.x[I, J]
                   for I in items for J in items
                   if not np.isnan(get_similarity(I, J, similarity))]
         return grb.quicksum(values)
 
-    def get_identity_similarity(self, tracks, identities,
-                                similarity, get_similarity):
+    def get_bipartite_similarity(self, items, otherItems,
+                                 similarity, get_similarity):
+        """Bi-partite similarity: ∑  xij.pij
+                                 i∈I
+                                 j∈J
         """
-         ∑  xti.pti
-        t,i
-        """
-        values = [get_similarity(T, I, similarity)*self.x[T, I]
-                  for T in tracks for I in identities
-                  if not np.isnan(get_similarity(T, I, similarity))]
+        values = [get_similarity(I, J, similarity)*self.x[I, J]
+                  for I in items for J in otherItems
+                  if not np.isnan(get_similarity(I, J, similarity))]
         return grb.quicksum(values)
 
-    def get_identity_dissimilarity(self, tracks, identities,
-                                   similarity, get_similarity):
+    def get_bipartite_dissimilarity(self, items, otherItems,
+                                    similarity, get_similarity):
+        """Bi-partite dissimilarity: ∑ (1-xij).(1-pij)
+                                    i∈I
+                                    j∈J
         """
-         ∑ (1-xti).(1-pti)
-        t,i
-        """
-        values = [(1-get_similarity(T, I, similarity))*(1-self.x[T, I])
-                  for T in tracks for I in identities
-                  if not np.isnan(get_similarity(T, I, similarity))]
+        values = [(1-get_similarity(I, J, similarity))*(1-self.x[I, J])
+                  for I in items for J in otherItems
+                  if not np.isnan(get_similarity(I, J, similarity))]
         return grb.quicksum(values)
 
     def solve(self, init=None,
@@ -305,6 +268,10 @@ class ILPClusteringMixin(object):
         for key, variable in self.x.iteritems():
             solution[key] = variable.x
 
+        return solution
+
+    def to_annotation(self, solution, modality, uri):
+
         # convert solution to list of clusters
         c = nx.Graph()
         for (I, J), same_cluster in solution.iteritems():
@@ -312,11 +279,32 @@ class ILPClusteringMixin(object):
             c.add_node(J)
             if same_cluster:
                 c.add_edge(I, J)
+
         clusters = nx.connected_components(c)
 
-        return clusters
+        annotation = Annotation(uri=uri, modality=modality)
+        for cluster in clusters:
 
-    def dump(self, path):
+            # obtain cluster identity
+            inodes = [node for node in cluster
+                      if isinstance(node, IdentityNode)]
+            if len(inodes) > 1:
+                raise ValueError('Cluster contains more than one identity.')
+            if inodes:
+                identity = inodes[0].identifier
+            else:
+                identity = Unknown()
+
+            # obtain cluster tracks
+            tnodes = [node for node in cluster if isinstance(node, TrackNode)
+                      and node.uri == uri and node.modality == modality]
+
+            for node in tnodes:
+                annotation[node.segment, node.track] = identity
+
+        return annotation
+
+    def dump_model(self, path):
         """
         Dump Gurobi model to file (for debugging purpose)
 
@@ -334,42 +322,43 @@ class ILPClusteringMixin(object):
         self.model.write(path)
 
 
-class FinkelConstraintMixin(ILPClusteringMixin):
+class Finkel2008(ILPClustering):
     """
 
+    Maximize ∑  α.xij.pij + (1-α).(1-xij).(1-pij)
+            i,j
+
+    References
+    ----------
+    * "Enforcing Transitivity in Coreference Resolution"
+      J.R. Finkel and C.D. Manning
+      Annual Meeting of the Association for Computational Linguistics:
+      Human Language Technologies (ACL HLT), 2008.
+    * ""
+
     """
 
-    def set_contraints(self, items, similarity, get_similarity, debug=False):
+    def __init__(self, items, similarity, get_similarity, debug=False):
 
-        if debug:
-            sys.stderr.write("DEBUG: add reflexivity constraints\n")
-        self.add_reflexivity_constraints(items)
+        super(Finkel2008, self).__init__()
 
-        if debug:
-            sys.stderr.write("DEBUG: add hard constraints\n")
-        self.add_hard_constraints(items, similarity, get_similarity)
+        self.items = items
+        self.similarity = similarity
+        self.get_similarity = get_similarity
 
-        if debug:
-            sys.stderr.write("DEBUG: add symmetry constraints\n")
-        self.add_symmetry_constraints(items)
+        # Variables
+        self.add_pair_variables(self.items)
 
-        if debug:
-            sys.stderr.write("DEBUG: add transitivity constraints\n")
-        self.add_transitivity_constraints(items)
+        # Constraints
+        self.add_reflexivity_constraints(self.items)
+        self.add_hard_constraints(self.items, self.similarity,
+                                  self.get_similarity)
+        self.add_symmetry_constraints(self.items)
+        self.add_transitivity_constraints(self.items)
 
         self.model.update()
 
-        return self
-
-
-class INTRAinterObjectiveMixin(object):
-    """
-    Maximize ∑  α.xij.pij + (1-α).(1-xij).(1-pij)
-            i,j
-    """
-
-    def set_objective(self, items, similarity, get_similarity,
-                      alpha=0.5, **kwargs):
+    def set_objective(self, alpha=0.5, **kwargs):
         """
         Set objective function
 
@@ -380,109 +369,95 @@ class INTRAinterObjectiveMixin(object):
 
         """
 
-        # intra-cluster similarity
-        intraSim = grb.quicksum(
-            [get_similarity(I, J, similarity)*self.x[I, J] for (I, J) in self.x
-             if not np.isnan(get_similarity(I, J, similarity))])
-
-        # inter-cluster similarity
-        interSim = grb.quicksum(
-            [(1-get_similarity(I, J, similarity))*(1-self.x[I, J])
-             for (I, J) in self.x
-             if not np.isnan(get_similarity(I, J, similarity))])
-
-        # jointly maximize intra-cluster similarity
-        # and minimize inter-cluster one
-        self.model.setObjective(alpha*intraSim + (1-alpha)*interSim,
-                                grb.GRB.MAXIMIZE)
-
-        # model (lazy) update
+        intra = self.get_intra_cluster_similarity(self.items, self.similarity,
+                                                  self.get_similarity)
+        inter = self.get_inter_cluster_dissimilarity(self.items,
+                                                     self.similarity,
+                                                     self.get_similarity)
+        self.model.setObjective(alpha*intra+(1-alpha)*inter, grb.GRB.MAXIMIZE)
         self.model.update()
 
-        return self
+# class DupuyConstraintMixin(object):
+#     """
+
+#     Dupuy et al. ...
+#     """
+
+#     def set_constraints(self, items, similarity, get_similarity,
+#                         delta=0.5, **kwargs):
+#         """
+
+#         Parameters
+#         ----------
+#         items :
+
+#         similarity :
+
+#         get_similarity :
+
+#         delta : float, optional
+#             Prevent items with similarity lower than delta from ending
+#             in the same cluster. Must fall in [0, 1] range.
+
+#         """
+#         # Equation 1.3 (in Dupuy et al., JEP'12)
+#         # every item is associated to exactly one centroid
+#         for J in items:
+#             constr = grb.quicksum([self.x[C, J] for C in items]) == 1
+#             self.model.addConstr(constr)
+
+#         # Equation 1.4 (in Dupuy et al., JEP'12)
+#         # prevent items from being associated to a dissimilar centroid
+#         for C in items:
+#             for I in items:
+#                 sCI = get_similarity(C, I, similarity)
+#                 if np.isnan(sCI):
+#                     continue
+#                 constr = (1-sCI) * self.x[C, I] <= (1-delta)
+#                 self.model.addConstr(constr)
+
+#         # Equation 1.5 (missing in Dupuy et al.)
+#         # activate a centroid as soon as an item is associated to it
+#         for C in items:
+#             for I in items:
+#                 constr = self.x[C, C] >= self.x[C, I]
+#                 self.model.addConstr(constr)
+
+#         # model (lazy) update
+#         self.model.update()
+
+#         return self
 
 
-class DupuyConstraintMixin(object):
-    """
+# class DupuyObjectiveMixin(object):
+#     """
 
-    Dupuy et al. ...
-    """
+#     """
 
-    def set_constraints(self, items, similarity, get_similarity,
-                        delta=0.5, **kwargs):
-        """
+#     def set_objective(self, items, similarity, get_similarity, **kwargs):
+#         """
 
-        Parameters
-        ----------
-        items :
+#         """
 
-        similarity :
+#         # number of items
+#         N = len(items)
 
-        get_similarity :
+#         # number of activated centroids
+#         centroids = grb.quicksum([self.x[C, C] for C in items])
 
-        delta : float, optional
-            Prevent items with similarity lower than delta from ending
-            in the same cluster. Must fall in [0, 1] range.
+#         # cluster cohesion (ie total similarity to centroids)
+#         cohesion = grb.quicksum([get_similarity(C, I, similarity)*self.x[C, I]
+#                                  for C in items for I in items if C != I])
 
-        """
-        # Equation 1.3 (in Dupuy et al., JEP'12)
-        # every item is associated to exactly one centroid
-        for J in items:
-            constr = grb.quicksum([self.x[C, J] for C in items]) == 1
-            self.model.addConstr(constr)
+#         # according to a discussion I had with Mickael Rouvier,
+#         # F (in Dupuy et al. 2012) is actually the sum over all items
+#         # of the maximum distance to all other items
+#         # in short, F = N
 
-        # Equation 1.4 (in Dupuy et al., JEP'12)
-        # prevent items from being associated to a dissimilar centroid
-        for C in items:
-            for I in items:
-                sCI = get_similarity(C, I, similarity)
-                if np.isnan(sCI):
-                    continue
-                constr = (1-sCI) * self.x[C, I] <= (1-delta)
-                self.model.addConstr(constr)
+#         # minimize both number of centroids and dispersion
+#         self.model.setObjective(centroids - 1./N * cohesion, grb.GRB.MINIMIZE)
 
-        # Equation 1.5 (missing in Dupuy et al.)
-        # activate a centroid as soon as an item is associated to it
-        for C in items:
-            for I in items:
-                constr = self.x[C, C] >= self.x[C, I]
-                self.model.addConstr(constr)
+#         # model (lazy) update
+#         self.model.update()
 
-        # model (lazy) update
-        self.model.update()
-
-        return self
-
-
-class DupuyObjectiveMixin(object):
-    """
-
-    """
-
-    def set_objective(self, items, similarity, get_similarity, **kwargs):
-        """
-
-        """
-
-        # number of items
-        N = len(items)
-
-        # number of activated centroids
-        centroids = grb.quicksum([self.x[C, C] for C in items])
-
-        # cluster cohesion (ie total similarity to centroids)
-        cohesion = grb.quicksum([get_similarity(C, I, similarity)*self.x[C, I]
-                                 for C in items for I in items if C != I])
-
-        # according to a discussion I had with Mickael Rouvier,
-        # F (in Dupuy et al. 2012) is actually the sum over all items
-        # of the maximum distance to all other items
-        # in short, F = N
-
-        # minimize both number of centroids and dispersion
-        self.model.setObjective(centroids - 1./N * cohesion, grb.GRB.MINIMIZE)
-
-        # model (lazy) update
-        self.model.update()
-
-        return self
+#         return self
