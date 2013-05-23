39f98d16c05440bbc11b31dc188bdde905666693
Hervé BREDIN <bredin@limsi.fr>
2013-05-23 15:02:11 +0200

    PEP 8

--- pyannote/base/feature.py
+++ pyannote/base/feature.py
@@ -4,17 +4,17 @@
 # Copyright 2012 Herve BREDIN (bredin@limsi.fr)
 
 # This file is part of PyAnnote.
-# 
+#
 #     PyAnnote is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     PyAnnote is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -30,32 +30,32 @@ from pyannote.base.timeline import Timeline
 
 class BaseSegmentFeature(object):
     """
-    Base class for any segment/feature iterator.    
+    Base class for any segment/feature iterator.
     """
     def __init__(self, uri=None):
         super(BaseSegmentFeature, self).__init__()
         self.__uri = uri
-    
-    def __get_uri(self): 
+
+    def __get_uri(self):
         return self.__uri
     def __set_uri(self, value):
         self.__uri = value
     uri = property(fget=__get_uri, fset=__set_uri)
     """Path to (or any identifier of) described resource"""
-    
+
     def __iter__(self):
         """Segment/feature vector iterator
-        
+
         Use expression 'for segment, feature_vector in segment_feature'
-        
+
         This method must be implemented by subclass.
-        
+
         """
         raise NotImplementedError('Missing method "__iter__".')
- 
+
 class BasePrecomputedSegmentFeature(BaseSegmentFeature):
     """'Segment iterator'-driven precomputed feature iterator.
-    
+
     Parameters
     ----------
     data : numpy array
@@ -65,17 +65,17 @@ class BasePrecomputedSegmentFeature(BaseSegmentFeature):
         Its length must correspond to `data` length.
     uri : string, optional
         name of (audio or video) described resource
-    
+
     """
     def __init__(self, data, segment_iterator, uri=None):
         # make sure data does not contain NaN nor inf
         data = np.asarray_chkfinite(data)
-                
+
         # make sure segment_iterator is actually one of those
         if not isinstance(segment_iterator, (SlidingWindow, Timeline)):
             raise TypeError("segment_iterator must 'Timeline' or "
                             "'SlidingWindow'.")
-        
+
         # make sure it iterates the correct number of segments
         try:
             N = len(segment_iterator)
@@ -89,94 +89,94 @@ class BasePrecomputedSegmentFeature(BaseSegmentFeature):
             if n != N:
                 raise ValueError("mismatch between number of segments (%d) "
                                  "and number of feature vectors (%d)." % (N, n))
-        
+
         super(BasePrecomputedSegmentFeature, self).__init__(uri=uri)
         self.__data = data
         self._segment_iterator = segment_iterator
-    
-    def __get_data(self): 
+
+    def __get_data(self):
         return self.__data
     data = property(fget=__get_data)
     """Raw feature data (numpy array)"""
 
     def __iter__(self):
         """Feature vector iterator
-        
+
         Use expression 'for segment, feature_vector in periodic_feature'
-        
+
         """
-        
-        # get number of feature vectors 
+
+        # get number of feature vectors
         n = self.__data.shape[0]
-        
+
         for i, segment in enumerate(self._segment_iterator):
-            
+
             # make sure we do not iterate too far...
             if i >= n:
                 break
-            
+
             # yield current segment and corresponding feature vector
             yield segment, self.__data[i]
-    
+
     def _segmentToRange(self, segment):
         """
         Parameters
         ----------
         segment : :class:`pyannote.base.segment.Segment`
-        
+
         Returns
         -------
         i, n : int
-        
+
         """
         raise NotImplementedError('Missing method "_segmentToRange".')
-    
+
     def _rangeToSegment(self, i, n):
         """
         Parameters
         ----------
         i, n : int
-        
+
         Returns
         -------
         segment : :class:`pyannote.base.segment.Segment`
-        
+
         """
         raise NotImplementedError('Missing method "_rangeToSegment".')
-        
+
     def __call__(self, subset, mode='loose'):
         """
         Use expression 'feature(subset)'
-        
+
         Parameters
         ----------
-        subset : :class:`pyannote.base.segment.Segment` or 
+        subset : :class:`pyannote.base.segment.Segment` or
                  :class:`pyannote.base.timeline.Timeline`
-        
+
         Returns
         -------
         data : numpy array
-        
+
         """
-    
+
         if not isinstance(subset, (Segment, Timeline)):
             raise TypeError('')
-        
+
         if isinstance(subset, Segment):
             i, n = self._segmentToRange(subset)
             indices = range(i, i+n)
-        
+
         elif isinstance(subset, Timeline):
             indices = []
             for segment in subset.coverage():
                 i, n = self._segmentToRange(segment)
                 indices += range(i, i+n)
-                
+
         return np.take(self.__data, indices, axis=0, out=None, mode='clip')
-    
+
 class PeriodicPrecomputedFeature(BasePrecomputedSegmentFeature):
     """'Sliding window'-driven precomputed feature iterator.
-    
+
     Parameters
     ----------
     data : numpy array
@@ -186,7 +186,7 @@ class PeriodicPrecomputedFeature(BasePrecomputedSegmentFeature):
         (or it can be infinite -- ie. sliding_window.end = None)
     uri : string, optional
         name of (audio or video) described resource
-    
+
     Examples
     --------
         >>> data = ...
@@ -194,48 +194,48 @@ class PeriodicPrecomputedFeature(BasePrecomputedSegmentFeature):
         >>> feature_iterator = PeriodicPrecomputedFeature(data, sliding_window)
         >>> for segment, feature_vector in feature_iterator:
         ...     pass
-        
+
     """
-    
+
     def __init__(self, data, sliding_window, uri=None):
-        
+
         super(PeriodicPrecomputedFeature, self).__init__(data, sliding_window, \
                                                          uri=uri)
-    
-    def __get_sliding_window(self): 
+
+    def __get_sliding_window(self):
         return self._segment_iterator
     sliding_window = property(fget=__get_sliding_window)
-    
-    
+
+
     def _segmentToRange(self, segment):
         """
         Parameters
         ----------
         segment : :class:`pyannote.base.segment.Segment`
-        
+
         Returns
         -------
         i, n : int
-        
+
         """
         return self.sliding_window.segmentToRange(segment)
-    
+
     def _rangeToSegment(self, i, n):
         """
         Parameters
         ----------
         i, n : int
-        
+
         Returns
         -------
         segment : :class:`pyannote.base.segment.Segment`
-        
+
         """
         return self.sliding_window.rangeToSegment(i, n)
 
 class TimelinePrecomputedFeature(BasePrecomputedSegmentFeature):
     """Timeline-driven precomputed feature iterator.
-    
+
     Parameters
     ----------
     data : numpy array
@@ -244,7 +244,7 @@ class TimelinePrecomputedFeature(BasePrecomputedSegmentFeature):
         Timeline whose length must correspond to `data` length
     uri : string, optional
         name of (audio or video) described resource
-    
+
     Examples
     --------
         >>> data = ...
@@ -252,18 +252,18 @@ class TimelinePrecomputedFeature(BasePrecomputedSegmentFeature):
         >>> feature_iterator = TimelinePrecomputedFeature(data, timeline)
         >>> for segment, feature_vector in feature_iterator:
         ...     pass
-    
-    
+
+
     """
-    
-    def __init__(self, data, timeline, uri=None):  
+
+    def __init__(self, data, timeline, uri=None):
         super(TimelinePrecomputedFeature, self).__init__(data, timeline, \
                                                          uri=uri)
-    
-    def __get_timeline(self): 
+
+    def __get_timeline(self):
         return self._segment_iterator
     timeline = property(fget=__get_timeline)
-    
+
     def _segmentToRange(self, segment):
         timeline = self.timeline.crop(segment, mode='loose')
         if timeline:
@@ -277,11 +277,11 @@ class TimelinePrecomputedFeature(BasePrecomputedSegmentFeature):
             n = 0
 
         return i, n
-        
+
     def _rangeToSegment(self, i, n):
         first_segment = self.timeline[i]
         last_segment = self.timeline[i+n]
-        return first_segment | last_segment    
+        return first_segment | last_segment
 
 if __name__ == "__main__":
     import doctest
