d26db7ae84048e5650aca29ffec6bc0ba578140e
Hervé BREDIN <bredin@limsi.fr>
2013-08-21 16:29:27 +0200

    style(util): PEP8

--- pyannote/util.py
+++ pyannote/util.py
@@ -1,10 +1,9 @@
 import warnings
 from decorator import decorator
 
+
 def deprecated(replacedByFunc):
     def _d(f, *args, **kwargs):
         warnings.warn('"%s" is deprecated. use "%s" instead.' % (f.func_name, replacedByFunc.__name__))
         return f(*args, **kwargs)
     return decorator(_d)
-
-    
