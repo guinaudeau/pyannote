d3247a562a5da78cef7a337ffda5bc8f9155ff1b
Hervé BREDIN <bredin@limsi.fr>
2013-08-30 17:27:26 +0200

    style(test): convert tabs to 4-spaces

--- test/test_base_segment.py
+++ test/test_base_segment.py
@@ -20,79 +20,79 @@
 
 from pyannote import Segment
 
+
 class test_base_segment(object):
 
-	def setup(self):
-		self.s1 = Segment(start=1, end=3)
-		self.s2 = Segment(start=2, end=4)
-		self.s3 = Segment(start=3, end=5)
-		self.s4 = Segment(start=4, end=6)
-		self.s5 = Segment(start=5, end=7)
+    def setup(self):
+        self.s1 = Segment(start=1, end=3)
+        self.s2 = Segment(start=2, end=4)
+        self.s3 = Segment(start=3, end=5)
+        self.s4 = Segment(start=4, end=6)
+        self.s5 = Segment(start=5, end=7)
 
-	def teardown(self):
-		pass
+    def teardown(self):
+        pass
 
-	def test_precision(self):
-		precision = 1e-6
-		assert not Segment(start=1, end=1+precision)
-		assert Segment(start=1, end=1+2*precision)
+    def test_precision(self):
+        precision = 1e-6
+        assert not Segment(start=1, end=1+precision)
+        assert Segment(start=1, end=1+2*precision)
 
-	def test_duration(self):
-		assert self.s2.duration == 2
+    def test_duration(self):
+        assert self.s2.duration == 2
 
-	def test_middle(self):
-		assert self.s2.middle == 3
+    def test_middle(self):
+        assert self.s2.middle == 3
 
-	def test_copy(self):
-		s = self.s2.copy()
-		assert self.s2 == s
-		assert id(self.s2) != s
+    def test_copy(self):
+        s = self.s2.copy()
+        assert self.s2 == s
+        assert id(self.s2) != s
 
-	def test_contains(self):
-		s = Segment(start=2, end=6)
-		assert not self.s1 in s
-		assert self.s2 in s
-		assert self.s3 in s
-		assert self.s4 in s
-		assert not self.s5 in s
+    def test_contains(self):
+        s = Segment(start=2, end=6)
+        assert not self.s1 in s
+        assert self.s2 in s
+        assert self.s3 in s
+        assert self.s4 in s
+        assert not self.s5 in s
 
-	def test_and(self):
-		assert self.s2 & self.s1 == Segment(start=2, end=3)
-		assert self.s2 & self.s2 == Segment(start=2, end=4)
-		assert self.s2 & self.s3 == Segment(start=3, end=4)
-		assert not (self.s2 & self.s4)
-		assert not (self.s2 & self.s5)
+    def test_and(self):
+        assert self.s2 & self.s1 == Segment(start=2, end=3)
+        assert self.s2 & self.s2 == Segment(start=2, end=4)
+        assert self.s2 & self.s3 == Segment(start=3, end=4)
+        assert not (self.s2 & self.s4)
+        assert not (self.s2 & self.s5)
 
-	def test_intersects(self):
-		assert self.s2.intersects(self.s1)
-		assert self.s2.intersects(self.s2)
-		assert self.s2.intersects(self.s3)
-		assert not self.s2.intersects(self.s4)
-		assert not self.s2.intersects(self.s5)
+    def test_intersects(self):
+        assert self.s2.intersects(self.s1)
+        assert self.s2.intersects(self.s2)
+        assert self.s2.intersects(self.s3)
+        assert not self.s2.intersects(self.s4)
+        assert not self.s2.intersects(self.s5)
 
-	def test_overlaps(self):
-		assert not self.s1.overlaps(5)
-		assert not self.s2.overlaps(5)
-		assert self.s3.overlaps(5)
-		assert self.s4.overlaps(5)
-		assert self.s5.overlaps(5)
+    def test_overlaps(self):
+        assert not self.s1.overlaps(5)
+        assert not self.s2.overlaps(5)
+        assert self.s3.overlaps(5)
+        assert self.s4.overlaps(5)
+        assert self.s5.overlaps(5)
 
-	def test_or(self):
-		assert self.s2 | self.s1 == Segment(start=1, end=4)
-		assert self.s2 | self.s2 == Segment(start=2, end=4)
-		assert self.s2 | self.s3 == Segment(start=2, end=5)
-		assert self.s2 | self.s4 == Segment(start=2, end=6)
-		assert self.s2 | self.s5 == Segment(start=2, end=7)
+    def test_or(self):
+        assert self.s2 | self.s1 == Segment(start=1, end=4)
+        assert self.s2 | self.s2 == Segment(start=2, end=4)
+        assert self.s2 | self.s3 == Segment(start=2, end=5)
+        assert self.s2 | self.s4 == Segment(start=2, end=6)
+        assert self.s2 | self.s5 == Segment(start=2, end=7)
 
-	def test_xor(self):
-		assert not (self.s2 ^ self.s1)
-		assert not (self.s2 ^ self.s2)
-		assert not (self.s2 ^ self.s3)
-		assert not (self.s2 ^ self.s4)
-		assert self.s2 ^ self.s5 == Segment(start=4, end=5)
+    def test_xor(self):
+        assert not (self.s2 ^ self.s1)
+        assert not (self.s2 ^ self.s2)
+        assert not (self.s2 ^ self.s3)
+        assert not (self.s2 ^ self.s4)
+        assert self.s2 ^ self.s5 == Segment(start=4, end=5)
 
-	def test_str(self):
-		assert str(self.s1) == '[1.000 --> 3.000]'
-		assert str(Segment(start=1.2345, end=5.6789)) == '[1.234 --> 5.679]'
-
+    def test_str(self):
+        assert str(self.s1) == '[1.000 --> 3.000]'
+        assert str(Segment(start=1.2345, end=5.6789)) == '[1.234 --> 5.679]'
 
