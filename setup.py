3a5c8ce8de85445c9e06897969e700a10a7a0fb4
Hervé BREDIN <bredin@limsi.fr>
2013-10-24 17:32:51 +0200

    chore: missing dependencies pysrt and joblib

--- setup.py
+++ setup.py
@@ -19,12 +19,16 @@
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='PyAnnote',
     version='0.3',
-    description='Python module for collaborative annotation of multimedia content',
+    description=(
+        'Python module for collaborative annotation '
+        'of multimedia content'
+    ),
     author='Hervé Bredin',
     author_email='bredin@limsi.fr',
     url='http://packages.python.org/PyAnnote',
@@ -39,17 +43,21 @@ setup(
     #           'pyannote.parser',
     #           'pyannote.parser.repere',
     #           'pyannote.parser.nist'],
-    install_requires=['numpy >=1.7.1',
-                      'scipy >=0.12.0',
-                      'banyan >=0.1.5',
-                      'pandas >=0.12.0',
-                      'munkres >=1.0.5.4',
-                      'decorator >=3.4.0',
-                      'networkx >=1.8.1',
-                      'scikit-learn >=0.14'],
-                      # 'lxml >=2.3.4'],
-                      #'prettytable >=0.6',
-                      #'python-Levenshtein >=0.10.2'],
+    install_requires=[
+        'numpy >=1.7.1',
+        'scipy >=0.12.0',
+        'banyan >=0.1.5',
+        'pandas >=0.12.0',
+        'munkres >=1.0.5.4',
+        'decorator >=3.4.0',
+        'networkx >=1.8.1',
+        'scikit-learn >=0.14',
+        'joblib >=0.7.1',
+        'pysrt >=0.5.0'
+        # 'lxml >=2.3.4'],
+        #'prettytable >=0.6',
+        #'python-Levenshtein >=0.10.2'],
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
