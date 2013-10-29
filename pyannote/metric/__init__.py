c12b7e7ac1816deb1572596b85aaf06a9b65d622
Hervé BREDIN <bredin@limsi.fr>
2013-10-29 17:10:40 +0100

    chore: do not import segmentation metrics

--- pyannote/metric/__init__.py
+++ pyannote/metric/__init__.py
@@ -4,17 +4,17 @@
 # Copyright 2012 Herve BREDIN (bredin@limsi.fr)
 
 # This file is part of PyAnnote.
-# 
+#
 #     PyAnnote is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     PyAnnote is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with PyAnnote.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -30,22 +30,22 @@ an aggregated error and confidence intervals. It is as easy as:
     >>> aggregated_value = abs(metric)
     >>> mean, (lower_bound, upper_bound) = metric.confidence_interval()
 
-See :class:`pyannote.metric.base.BaseMetric` for details on how to contribute 
+See :class:`pyannote.metric.base.BaseMetric` for details on how to contribute
 your own metrics.
 
 """
 
 
-__all__ = ['DetectionErrorRate', \
-           'DiarizationErrorRate', \
-           'IdentificationErrorRate', \
-           'SegmentationPrecision', 'SegmentationRecall', \
-           'f_measure']
+__all__ = [
+    'DetectionErrorRate',
+    'DiarizationErrorRate',
+    'IdentificationErrorRate',
+    'f_measure'
+]
 
 from detection import DetectionErrorRate
 from diarization import DiarizationErrorRate
 from identification import IdentificationErrorRate
-from segmentation import SegmentationPrecision, SegmentationRecall
 from base import f_measure
 
 if __name__ == "__main__":
